183cba98ddc2c6a01f2797852e68319fc45d9672
Alperen Gönül <kamilcanalper20@gmail.com>
2021-04-20 01:53:18 +0300

    xml uzantısı ekleme ve değerleri çağırma

--- Form1.cs
+++ Form1.cs
@@ -6,15 +6,48 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 using System.Windows.Forms;
 
 namespace xml_vize
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public  Form1()
         {
             InitializeComponent();
+
+        }
+
+       string xmluznt = "https://www.mgm.gov.tr/FTPDATA/analiz/sonSOA.xml";
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlDocument hvdrm = new XmlDocument();
+            hvdrm.Load(xmluznt);
+            XmlElement root = hvdrm.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("sehirler");
+
+            foreach (XmlNode node in nodes)
+            {
+                string ili = node["ili"].InnerText;
+                string durum = node["Durum"].InnerText;
+                string maks = node["Mak"].InnerText;
+                string min = node["Min"].InnerText;
+               
+                   
+                if (ili == "İstanbul")
+                {
+                    MessageBox.Show("durumu'{}'",durum);
+                }
+            
+            }
+        
         }
     }
 }
