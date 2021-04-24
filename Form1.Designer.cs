1ff5b995f08f46db373cc20e3cbc96be2a9a9b6f
Alperen Gönül <kamilcanalper20@gmail.com>
2021-04-24 16:25:45 +0300

    veri çekme bildirim verme eklendi görüntü düzeltildi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -51,6 +51,10 @@ namespace xml_vize
             this.canakkale = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.manisa)).BeginInit();
@@ -301,6 +305,43 @@ namespace xml_vize
             this.label1.TabIndex = 29;
             this.label1.Text = "label1";
             // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label2.Location = new System.Drawing.Point(1568, 597);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 39);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(1557, 667);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Aqua;
+            this.label4.Location = new System.Drawing.Point(1627, 667);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "label4";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -308,6 +349,9 @@ namespace xml_vize
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1794, 753);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.canakkale);
             this.Controls.Add(this.artvin);
@@ -374,6 +418,10 @@ namespace xml_vize
         private System.Windows.Forms.PictureBox canakkale;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,7 +17,7 @@ namespace xml_vize
     public partial class Form1 : Form
     {
 
-       public const string havadurumu_link = "https://www.mgm.gov.tr/FTPDATA/analiz/sonSOA.xml";
+        public const string havadurumu_link = "https://www.mgm.gov.tr/FTPDATA/analiz/sonSOA.xml";
 
         public Form1()
         {
@@ -27,34 +27,71 @@ namespace xml_vize
 
         public void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Sıcaklığını ve durumunu öğrenmek istediğiniz ilin üzerindeki raptiye fotğrafına tıklayınız");
-            MessageBox.Show("Sıcaklık ve durum değerlendirmeleri hergün saat 18:00'da güncellenmektedir");
-
-        
-            timer1.Start();
-            A:
-            XDocument deneme = XDocument.Load(havadurumu_link);
-
-            //ist
+             MessageBox.Show("Sıcaklığını ve durumunu öğrenmek istediğiniz ilin üzerindeki raptiye fotğrafına tıklayınız");
+            MessageBox.Show("Sıcaklık ve durum değerlendirmeleri hergün saat 18:00 ve 06:00'da güncellenmektedir");
             
+            timer1.Start();
+            timer2.Start();
+           
+           
+            
+        }
+        
+        public void program()
+        {
+            
+            timer2.Interval = 1800000;
+            timer2.Enabled = true;
+           
+            XDocument deneme = XDocument.Load(havadurumu_link);
+            
+
+           
+            string zaman = deneme.Descendants("Tarih").ElementAt(0).Value;
+            string istanbul = deneme.Descendants("ili").ElementAt(0).Value;
             string istmax = deneme.Descendants("Mak").ElementAt(0).Value;
+            string istmin = deneme.Descendants("Min").ElementAt(0).Value;
+            string kocmax = deneme.Descendants("Mak").ElementAt(1).Value;
+            string kocmin = deneme.Descendants("Min").ElementAt(1).Value;
+            string canmax = deneme.Descendants("Mak").ElementAt(2).Value;
+            string canmin = deneme.Descendants("Min").ElementAt(2).Value;
+            string manmax = deneme.Descendants("Mak").ElementAt(29).Value;
+            string manmin = deneme.Descendants("Min").ElementAt(29).Value;
+            string izmax = deneme.Descendants("Mak").ElementAt(3).Value;
+            string izmin = deneme.Descendants("Min").ElementAt(3).Value;
+            string mugmax = deneme.Descendants("Mak").ElementAt(5).Value;
+            string mugmin = deneme.Descendants("Min").ElementAt(5).Value;
+
+            FileStream kayit2 = new FileStream(@"C:\Users\Alperen\Desktop\ntp deneme\xml vize\xml vize\bilgikayit.txt", FileMode.Append, FileAccess.Write, FileShare.Write);
+            StreamWriter kayded = new StreamWriter(kayit2);
+            string saat = DateTime.Now.ToLongTimeString();
+            kayded.WriteLine("---------------------------------------------------------------------------");
+            kayded.WriteLine(" ");
+            kayded.WriteLine("TARİH:  " + zaman + "     SAAT:  " + saat);
+            kayded.WriteLine("--İL------MAXSIC---MİNSIC");
+            kayded.WriteLine("İstanbul   {0}°-----{1}°", istmax, istmin);
+            kayded.WriteLine("Kocaeli    {0}°-----{1}°", kocmax, kocmin);
+            kayded.WriteLine("Çanakkale  {0}°-----{1}°", canmax, canmin);
+            kayded.WriteLine("Manisa     {0}°-----{1}°", manmax, manmin);
+            kayded.WriteLine("İzmir      {0}°-----{1}°", izmax, izmin);
+            kayded.WriteLine("Muğla      {0}°-----{1}°", mugmax, mugmin);
+
+            kayded.Close();
+            string manisa = deneme.Descendants("ili").ElementAt(29).Value;
+           
+            label2.Text = manisa;
+            label3.Text = manmax + "°";
+            label4.Text = manmin + "°";
 
 
-            string yedek;
-            yedek = istmax;
-            if (istmax!=yedek)
-            {
-                MessageBox.Show("BİLGİLER GÜNCELLENDİ");
-                goto A;
-            }
+
 
         }
-
-
+        
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-
+            
             XDocument deneme = XDocument.Load(havadurumu_link);
 
             //ist
@@ -84,7 +121,7 @@ namespace xml_vize
         private void canakkale_Click(object sender, EventArgs e)
         {
             XDocument deneme = XDocument.Load(havadurumu_link);
-            string kcanakkale = deneme.Descendants("ili").ElementAt(2).Value;
+            string canakkale = deneme.Descendants("ili").ElementAt(2).Value;
             string canmax = deneme.Descendants("Mak").ElementAt(2).Value;
             string canmin = deneme.Descendants("Min").ElementAt(2).Value;
             string candurum = deneme.Descendants("Durum").ElementAt(2).Value;
@@ -95,7 +132,7 @@ namespace xml_vize
         private void manisa_Click(object sender, EventArgs e)
         {
             XDocument deneme = XDocument.Load(havadurumu_link);
-            string canakkale = deneme.Descendants("ili").ElementAt(29).Value;
+            string manisa = deneme.Descendants("ili").ElementAt(29).Value;
             string manmax = deneme.Descendants("Mak").ElementAt(29).Value;
             string manmin = deneme.Descendants("Min").ElementAt(29).Value;
             string mandurum = deneme.Descendants("Durum").ElementAt(29).Value;
@@ -247,13 +284,29 @@ namespace xml_vize
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            
+            
             label1.Text ="SAAT:"+ DateTime.Now.ToLongTimeString();
 
             if (DateTime.Now.ToLongTimeString()=="18:00:00")
             {
                 MessageBox.Show("BİLGİLER GÜNCELLENDİ");
-            }    
+            }
+
+            if (DateTime.Now.ToLongTimeString() == "06:00:00")
+            {
+                MessageBox.Show("BİLGİLER GÜNCELLENDİ");
+            }
         }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            program();
+            MessageBox.Show("Yarım saatlik veri kontrolü gerçekleşti");
+           
+        }
+
+        
     }
 
 }
